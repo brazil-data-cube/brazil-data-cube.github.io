6024e70a9982cda2769ab403d9865a9a1411cc8a
gqueiroz <gribeiro.mg@gmail.com>
2021-03-28 19:58:45 -0300

    update site

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2020',
+    VERSION: '2020-2021',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
